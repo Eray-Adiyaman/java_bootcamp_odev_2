25dbfbc30c1ac092b02e2be67c9996fa3a113ca6
Eray-Adiyaman <erayadiyaman@outlook.com>
2022-09-23 19:09:03 +0300

    Classes_RecapDemo

---
+++ recapClasses_Demo/src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+
+        DortIslem dortIslem = new DortIslem();
+
+        System.out.println(dortIslem.Topla(1,3));
+        System.out.println(dortIslem.Cikar(10,2));
+        System.out.println(dortIslem.Carp(6,7));
+        System.out.println(dortIslem.Bol(144,12));
+    }
+}
