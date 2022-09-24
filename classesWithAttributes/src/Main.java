b615f8131f3424b28abaf89ea1d9805eb10b0da9
Eray-Adiyaman <erayadiyaman@outlook.com>
2022-09-24 16:25:42 +0300

    getSet

--- classesWithAttributes/src/Main.java
+++ classesWithAttributes/src/Main.java
@@ -2,14 +2,19 @@ public class Main {
     public static void main(String[] args) {
 
         Product product = new Product();
-        product.name = "laptop";
-        product.id=1;
-        product.description="Asus laptop";
-        product.price=10000;
-        product.stockAmount=20;
+        product.setName("Laptop");
+        product.setId(1);
+        product.setDescription("Asus Laptop");
+        product.setPrice(10000);
+        product.setStockAmount(3);
 
+        System.out.println(product.getId());
+        System.out.println(product.getCode());
         System.out.println(product);
+
         ProductManager productManager = new ProductManager();
         productManager.Add(product);
+
+
     }
 }
