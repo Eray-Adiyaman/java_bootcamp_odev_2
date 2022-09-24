6a788e8e6e8b5e209835546632ef1d472daf911a
Eray-Adiyaman <erayadiyaman@outlook.com>
2022-09-24 16:46:36 +0300

    Constructors

--- classesWithAttributes/src/Main.java
+++ classesWithAttributes/src/Main.java
@@ -1,12 +1,17 @@
 public class Main {
     public static void main(String[] args) {
 
+        //init with constructor parameters
+        Product product2 = new Product(2,"PC","Prebuilt PC",15000,10,"black");
+
+        //init empty and fill with setters
         Product product = new Product();
         product.setName("Laptop");
         product.setId(1);
         product.setDescription("Asus Laptop");
         product.setPrice(10000);
         product.setStockAmount(3);
+        product.setColor("gray");
 
         System.out.println(product.getId());
         System.out.println(product.getCode());
@@ -15,6 +20,12 @@ public class Main {
         ProductManager productManager = new ProductManager();
         productManager.Add(product);
 
+
+        System.out.println(product2.getName());
+        System.out.println(product2.getCode());
+        System.out.println(product2);
+
+        productManager.Add(product2);
 
     }
 }
--- classesWithAttributes/src/Product.java
+++ classesWithAttributes/src/Product.java
@@ -1,4 +1,22 @@
 public class Product {
+
+
+    //constructor with parameters
+    public Product(int id, String name, String description, double price, int stockAmount, String color) {
+        System.out.println("Constructor with parameter run");
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.price = price;
+        this.stockAmount = stockAmount;
+        this.color = color;
+    }
+
+    //Constructor without parameters
+    public Product(){
+        System.out.println("Constructor without parameter run");
+    }
+
     //attributes  | fields
     private int id;
     private String name;
